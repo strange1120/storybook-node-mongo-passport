5242c723cec41efe379849d992c549090e80dffb
Sarah Strange <strange1120@gmail.com>
2020-07-04 21:34:55 -0400

    setting up routes and handlebar templates

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
+const path = require("path");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+// Login/Landing page
+router.get("/", (req, res) => {
+  res.render("login", {
+    layout: "login"
+  });
+});
+
+// Dashboard page
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+module.exports = router;
